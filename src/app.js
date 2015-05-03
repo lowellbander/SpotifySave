e129cb7a4ac372e9b7aa0cb9b2cf3db540c10d16
Lowell Bander <lowellbander@gmail.com>
2015-05-03 16:27:13 -0700

    gets current song name!

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
+var ajax = require('ajax');
+
 // Import the UI elements
 var UI = require('ui');
 
-// Create a simple Card
-var card = new UI.Card({
-  title: 'Hello World',
-  body: 'This is your first Pebble app!'
+// Show splash
+var splashCard = new UI.Card({
+  title: "Please Wait",
+  body: "Downloading..."
 });
+splashCard.show();
 
-// Display to the user
-card.show();
+var lastfmURL = 'http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=lowellbander&api_key=410af592466bfb635d96c11f77053117&format=json';
+
+// Download data
+ajax({url: lastfmURL, type: 'json'},
+ function(json) {
+   console.log('here\'s the response!');
+   
+   var lastfmTrack = json['recenttracks']['track'][0]['name'];
+   console.log(lastfmTrack);
+
+  // Use data to show a weather forecast Card
+  var resultsCard = new UI.Card({
+    title: lastfmTrack,
+    body: 'The current track'
+  });
+
+  // Show results, remove splash card
+  resultsCard.show();
+  splashCard.hide();
+  },
+  function(error) {
+    console.log('Ajax failed: ' + error);
+  }
+);
+
