e0f5504d51d0129ac0a55b0e66882b7214bd66ad
Lowell Bander <lowellbander@gmail.com>
2015-05-04 14:13:32 -0700

    @goom11 cleaned my code

--- src/app.js
+++ src/app.js
@@ -1,51 +1,56 @@
 var ajax = require('ajax');
 var UI = require('ui');
 
-// Show splash
-var splashCard = new UI.Card({
-  title: "Please Wait",
-  body: "Downloading..."
-});
-splashCard.show();
-splashCard.on('click', function() {
-  update();
-});
-
-  var resultsCard = new UI.Card({
-    title: 'loading',
-    body: 'loading'
-  });
-resultsCard.on('click', function() {
-  update();
-});
 
 var lastfmURL = 'http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=lowellbander&api_key=410af592466bfb635d96c11f77053117&format=json';
 
-var update = function () {
-  console.log('starting update');
-  
-// Download data
-ajax({url: lastfmURL, type: 'json'},
- function(json) {
-  console.log('update successful');
-   
-   var lastfmTrack = json['recenttracks']['track'][0];
-   var trackName = lastfmTrack['name'];
-   var trackArtist = lastfmTrack['artist']['#text'];
-   var trackAlbum = lastfmTrack['album']['#text'];
-   //console.log(lastfmTrack);
-
-  resultsCard.title(trackName);
-  resultsCard.body(trackArtist + ', ' + trackAlbum);
-
-  // Show results, remove splash card
-  resultsCard.show();
-  splashCard.hide();
-  },
-  function(error) {
-    console.log('Ajax failed: ' + error);
-  }
-  );
+UI.Card.prototype.update = function(title, body) {
+    this.title(title);
+    this.body(body);
 };
+
+var splashCard = new UI.Card({
+    title: "Please Wait",
+    body: "Downloading..."
+});
+
+var resultsCard = new UI.Card({
+    title: 'loading',
+    body: 'loading'
+});
+
+var update = function () {
+    console.log('starting update');
+  resultsCard.update('loading', 'loading');
+    
+    // Download data
+    var data = {url: lastfmURL, type: 'json'};
+    ajax(data, function(json) {
+        console.log('update successful');
+
+        var lastfmTrack = json.recenttracks.track[0];
+        var trackName = lastfmTrack.name;
+        var trackArtist = lastfmTrack.artist['#text'];
+        var trackAlbum = lastfmTrack.album['#text'];
+
+
+        resultsCard.update(trackName, trackArtist + ', ' + trackAlbum);
+        //resultsCard.title(trackName);
+        //resultsCard.body(trackArtist + ', ' + trackAlbum);
+
+        // Show results, remove splash card
+        resultsCard.show();
+        splashCard.hide();
+    }, function(error) {
+        console.log('Ajax failed: ' + error);
+    });
+};
+
+splashCard.on('click', update);
+
+resultsCard.on('click', update);
+
+
+splashCard.show();
 
 update();
