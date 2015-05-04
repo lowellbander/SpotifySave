8c0fa3a0137c5f93b796313a9cc1883952d94a1f
Lowell Bander <lowellbander@gmail.com>
2015-05-03 17:03:46 -0700

    updates on start and on click

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
 var ajax = require('ajax');
-
-// Import the UI elements
 var UI = require('ui');
 
 // Show splash
@@ -9,13 +7,27 @@ var splashCard = new UI.Card({
   body: "Downloading..."
 });
 splashCard.show();
+splashCard.on('click', function() {
+  update();
+});
+
+  var resultsCard = new UI.Card({
+    title: 'loading',
+    body: 'loading'
+  });
+resultsCard.on('click', function() {
+  update();
+});
 
 var lastfmURL = 'http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=lowellbander&api_key=410af592466bfb635d96c11f77053117&format=json';
 
+var update = function () {
+  console.log('starting update');
+  
 // Download data
 ajax({url: lastfmURL, type: 'json'},
  function(json) {
-   console.log('here\'s the response!');
+  console.log('update successful');
    
    var lastfmTrack = json['recenttracks']['track'][0];
    var trackName = lastfmTrack['name'];
@@ -23,11 +35,8 @@ ajax({url: lastfmURL, type: 'json'},
    var trackAlbum = lastfmTrack['album']['#text'];
    //console.log(lastfmTrack);
 
-  // Use data to show a weather forecast Card
-  var resultsCard = new UI.Card({
-    title: trackName,
-    body: trackArtist + ', ' + trackAlbum
-  });
+  resultsCard.title(trackName);
+  resultsCard.body(trackArtist + ', ' + trackAlbum);
 
   // Show results, remove splash card
   resultsCard.show();
@@ -36,5 +45,7 @@ ajax({url: lastfmURL, type: 'json'},
   function(error) {
     console.log('Ajax failed: ' + error);
   }
-);
+  );
+};
 
+update();
