219ec49e0a37f4b290772acc2e833edc19ac455e
Lowell Bander <lowellbander@gmail.com>
2015-05-04 16:49:55 -0700

    loads on start, cleaner code

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 var ajax = require('ajax');
 var UI = require('ui');
 
-
 var lastfmURL = 'http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=lowellbander&api_key=410af592466bfb635d96c11f77053117&format=json';
 
 UI.Card.prototype.update = function(title, body) {
@@ -9,48 +8,36 @@ UI.Card.prototype.update = function(title, body) {
     this.body(body);
 };
 
-var splashCard = new UI.Card({
-    title: "Please Wait",
-    body: "Downloading..."
-});
+var log = function (message) { console.log(message); };
 
-var resultsCard = new UI.Card({
-    title: 'loading',
-    body: 'loading'
+var mainCard = new UI.Card({
+    title: "loading ...",
+    body: ""
 });
 
 var update = function () {
-    console.log('starting update');
-  resultsCard.update('loading', 'loading');
+    log('starting update');
+    mainCard.update('loading ...', '');
     
     // Download data
     var data = {url: lastfmURL, type: 'json'};
     ajax(data, function(json) {
-        console.log('update successful');
+        log('update successful');
 
         var lastfmTrack = json.recenttracks.track[0];
         var trackName = lastfmTrack.name;
         var trackArtist = lastfmTrack.artist['#text'];
         var trackAlbum = lastfmTrack.album['#text'];
 
+        mainCard.update(trackName, trackArtist + ', ' + trackAlbum);
 
-        resultsCard.update(trackName, trackArtist + ', ' + trackAlbum);
-        //resultsCard.title(trackName);
-        //resultsCard.body(trackArtist + ', ' + trackAlbum);
-
-        // Show results, remove splash card
-        resultsCard.show();
-        splashCard.hide();
     }, function(error) {
-        console.log('Ajax failed: ' + error);
+        log('Ajax failed: ' + error);
     });
 };
 
-splashCard.on('click', update);
-
-resultsCard.on('click', update);
-
-
-splashCard.show();
-
 update();
+
+mainCard.show();
+
+mainCard.on('click', update);
