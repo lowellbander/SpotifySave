b6b4cadb53237098ab8854ae05aae7ffeb7db60e
Lowell Bander <lowellbander@gmail.com>
2015-05-04 17:01:26 -0700

    cleaner

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 var ajax = require('ajax');
 var UI = require('ui');
 
-var lastfmURL = 'http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=lowellbander&api_key=410af592466bfb635d96c11f77053117&format=json';
-
 UI.Card.prototype.update = function(title, body) {
     this.title(title);
     this.body(body);
 };
 
 var log = function (message) { console.log(message); };
+
+var currentTrackInfoFromJson = function (json) {
+    var track = json.recenttracks.track[0];
+    return {
+        name : track.name,
+        artist : track.artist['#text'],
+        album : track.album['#text']
+    };
+};
 
 var mainCard = new UI.Card({
     title: "loading ...",
@@ -19,21 +26,23 @@ var update = function () {
     log('starting update');
     mainCard.update('loading ...', '');
     
+    var lastfmURL = 'http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=lowellbander&api_key=410af592466bfb635d96c11f77053117&format=json';
+
     // Download data
     var data = {url: lastfmURL, type: 'json'};
     ajax(data, function(json) {
         log('update successful');
-
-        var lastfmTrack = json.recenttracks.track[0];
-        var trackName = lastfmTrack.name;
-        var trackArtist = lastfmTrack.artist['#text'];
-        var trackAlbum = lastfmTrack.album['#text'];
-
-        mainCard.update(trackName, trackArtist + ', ' + trackAlbum);
-
+        var track = currentTrackInfoFromJson(json);
+        mainCard.update(track.name, track.artist + ', ' + track.album);
     }, function(error) {
         log('Ajax failed: ' + error);
     });
+};
+
+var getSpotifyTrackID = function () {
+    var spotifySearchURL = 'https://api.spotify.com/v1/search?type=track&q='
+
+    
 };
 
 update();
